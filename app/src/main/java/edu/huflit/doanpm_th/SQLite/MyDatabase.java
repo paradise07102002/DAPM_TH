e08a681e0e7d1754e328c79599f0e716254ff70a
phamdinhkhoi <paradise07102002@gmail.com>
2023-10-27 00:52:52 +0700

    MyDatabase

--- app/src/main/java/edu/huflit/doanpm_th/SQLite/MyDatabase.java
+++ app/src/main/java/edu/huflit/doanpm_th/SQLite/MyDatabase.java
@@ -1,2 +1,15 @@
-package edu.huflit.doanpm_th.SQLite;public class MyDatabase {
+package edu.huflit.doanpm_th.SQLite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class MyDatabase {
+    SQLiteDatabase database;
+    DBHelper helper;
+
+    public MyDatabase(Context context)
+    {
+        helper = new DBHelper(context);
+        database = helper.getWritableDatabase();
+    }
 }
